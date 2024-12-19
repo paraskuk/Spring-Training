5744bc26ed880215572d0a07269e2f3b15c64322
Paraskevas Paraskevas <paraskevasp@gmail.com>
2024-12-19 20:27:05 +0000

    add prod dev configurations

--- src/main/java/org/example/config/ApplicationConfig.java
+++ src/main/java/org/example/config/ApplicationConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
@@ -25,8 +26,15 @@ public class ApplicationConfig {
     private TimeService timeService;
 
     @Bean
+    @Profile("!dev")
     public TimeService timeService(){
-        return new TimeService(is24);
+        return new TimeService(true);
+    }
+
+    @Bean
+    @Profile("dev")
+    public TimeService timeService12(){
+        return new TimeService(false);
     }
 
 
